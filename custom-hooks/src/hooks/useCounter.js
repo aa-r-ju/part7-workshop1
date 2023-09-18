c38a50be421a043f17ae86139e0ac712e7c18cd5
aa-r-ju <aarju3447@gmail.com>
2023-09-18 12:09:24 +0545

    fix:fixed part7 b video1

--- custom-hooks/src/hooks/useCounter.js
+++ custom-hooks/src/hooks/useCounter.js
@@ -1,5 +1,5 @@
 import { useState } from "react"
-const useCounter = () => {
+const useCounter = (initialValue = 0 ) => {
     const [value, setValue] = useState(0)
   
     const increase = () => {
